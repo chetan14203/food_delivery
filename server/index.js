9e44716872997412256ed23c04a3f532f62c61bf
chetan14203 <109654816+chetan14203@users.noreply.github.com>
2025-02-18 00:13:02 +0530

    Describe the changes you made

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ const connectDB = () => {
 const startServer = async () => {
   try {
     connectDB();
-    app.listen(8080, () => console.log("Server started on port 8080"));
+    app.listen(8000, () => console.log("Server started on port 8080"));
   } catch (error) {
     console.log(error);
   }
